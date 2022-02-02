5931e5061f0a84c770b776139d27a1e04cd3771a
mridul2820 <immridulpanda2820@gmail.com>
2022-02-02 14:14:53 +0530

    code structure update

--- lib/main.dart
+++ lib/main.dart
@@ -12,77 +12,80 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(
-                  'https://www.mridul.tech/_next/image?url=%2Fmridul.jpg&w=256&q=75'),
-            ),
-            Text(
-              'Mridul Panda',
-              style: TextStyle(
-                  fontFamily: 'Shadows Into Light',
-                  fontSize: 30,
-                  color: Colors.white,
-                  letterSpacing: 4,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'WEB & APP DEVELOPER',
-              style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://www.mridul.tech/_next/image?url=%2Fmridul.jpg&w=256&q=75'),
+              ),
+              Text(
+                'Mridul Panda',
+                style: TextStyle(
+                    fontFamily: 'Shadows Into Light',
+                    fontSize: 30,
+                    color: Colors.white,
+                    letterSpacing: 4,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'WEB & APP DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
                   color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-              width: 150,
-              child: Divider(
-                color: Colors.teal.shade100,
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                  size: 20,
-                ),
-                title: Text(
-                  '+91 9775559328',
-                  style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20,
-                      color: Colors.teal.shade900),
                 ),
               ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                  size: 24,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 20,
+                  ),
+                  title: Text(
+                    '+91 9775559328',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal.shade900),
+                  ),
                 ),
-                title: Text('contact@mridul.tech',
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 24,
+                  ),
+                  title: Text(
+                    'contact@mridul.tech',
                     style: TextStyle(
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20,
                         color: Colors.teal.shade900,
                         letterSpacing: 2.5,
-                        fontWeight: FontWeight.bold)),
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
               ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
